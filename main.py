3f8b1e2ce0a8eaee567db4ac4e3425dd52bfe56d
schmolioli <hkuikahi29@setoncatholic.org>
2026-09-23 20:14:10 +0000

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# this function adds two numbers
+def add(x,y):
+    print(x+y)
+
+# this function subtracts two numbers
+def sub(x,y):
+    print(x-y)
+
+# this function multiplies two numbers
+def multi(x,y):
+    print(x*y)
+
+# this function divides two numbers
+def div(x,y):
+    print(x/y)
+#################################################
+
+x=int(input("Enter your first number: "))
+y=int(input("Enter your second number: "))
+
+add(x,y)
+sub(x,y)
+multi(x,y)
+div(x,y)
